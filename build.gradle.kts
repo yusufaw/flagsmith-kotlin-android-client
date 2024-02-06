2bea13e6d7abe7ba679565c06b3d38bcc8f68525
Yusuf Aji Wibowo <yusuf.wibowo@mekari.com>
2024-02-06 08:59:46 +0100

    Update kotlin version from 1.8.0 to 1.6.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application").version("7.3.1").apply(false)
     id("com.android.library").version("7.3.1").apply(false)
-    kotlin("android").version("1.8.0").apply(false)
+    kotlin("android").version("1.6.10").apply(false)
     id("org.jetbrains.kotlinx.kover").version("0.6.1").apply(false)
 }
 
